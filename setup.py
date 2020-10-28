af85e38a5125a43c14c2cbd749265fce80e2854b
Thomas Gläßle <t_glaessle@gmx.de>
2020-10-28 23:51:28 +0100

    Move to src/ subdirectory
    To avoid renaming during tests and weird issue on older version of
    pip.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 meta = {}
-with open('minrpc/__init__.py', 'rb') as f:
+with open('src/minrpc/__init__.py', 'rb') as f:
     exec(f.read(), meta, meta)
 
 setup(
     name='minrpc',
     version=meta['__version__'],
+    package_dir={'': 'src'},
 )
