b69aabd0f34112329bf46e564e3b86e6d4359b96
Thomas Gläßle <t_glaessle@gmx.de>
2020-10-28 23:27:21 +0100

    Set name/version in setup.py
    This fixes some problems with earlier versions of setuptools.

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
 from setuptools import setup
-setup()
+
+meta = {}
+with open('minrpc/__init__.py', 'rb') as f:
+    exec(f.read(), meta, meta)
+
+setup(
+    name='minrpc',
+    version=meta['__version__'],
+)
